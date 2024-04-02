32064e5f7ae4173d6b6a65207816d09488b5d7ea
Isabela Escobar <163205304+isabelaed09@users.noreply.github.com>
2024-04-01 20:07:24 -0500

    Se definió diccionarios para los artículos disponibles

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,1 +1,18 @@
+camisetas = { 1: ["polo", "blanca", 15],
+             2: ["polo", "azul", 15],
+             3: ["polo", "roja", 15],
+             4: ["polo", "amarilla", 15],
+             5: ["cuello redondo", "gris", 12],
+             6: ["cuello redondo", "negro", 12],
+             7: ["cuello redondo", "verde", 12]}
 
+jeans = { 1: ["Azul", 20],
+         2: ["verde", 20],
+         3: ["cafe", 20],
+         4: ["negro", 20],
+         5: ["gris", 20]}
+
+zapatos = { 1: ["botas", "cafe", 25],
+           2: ["tenis", "azul", 20],
+           3: ["botas", "negro", 25],
+           4: ["tenis", "blanco", 20]}
